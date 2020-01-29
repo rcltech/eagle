d3fb196a414c6bb00fbda9e3959dd78b2ce2a503
seanchok <seanchok@gmail.com>
2020-01-29 15:43:26 +0800

    #4: add apollo client with persistent cache to index component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { theme } from "./config/theme";
 import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { persistCache } from "apollo-cache-persist";
+import { PersistentStorage, PersistedData } from "apollo-cache-persist/types";
 
 const Index: React.FC = () => {
+  const [client, setClient] = useState();
+
+  useEffect(() => {
+    // const authorization: string = localStorage.getItem("id");
+    const uri: string =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:4000/graphql"
+        : "https://phoenix.rctech.club/graphql";
+
+    const link = new HttpLink({
+      uri,
+      // headers: {
+      //   authorization,
+      // },
+    });
+
+    const cache = new InMemoryCache();
+
+    const client = new ApolloClient({
+      cache,
+      link,
+    });
+
+    persistCache({
+      cache,
+      storage: window.sessionStorage as PersistentStorage<
+        PersistedData<NormalizedCacheObject>
+      >,
+    }).then(() => setClient(client));
+  });
+
+  if (!client) return <>loading</>;
+
   return (
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
