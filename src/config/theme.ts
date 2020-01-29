46b45a5cb9d8ebdb90ad6e48750b8c19130eb552
seanchok <seanchok@gmail.com>
2020-01-29 15:26:43 +0800

    #5: setup mui theme in config and test in App component

---
+++ src/config/theme.ts
@@ -0,0 +1,22 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+export const theme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      primary: {
+        light: "#727394",
+        main: "#464866",
+        dark: "#1d213b",
+      },
+      secondary: {
+        main: "#ef9a9a",
+      },
+      background: {
+        default: "#fff",
+      },
+      error: {
+        main: "#B00020",
+      },
+    },
+  })
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { theme } from "./config/theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index: React.FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
